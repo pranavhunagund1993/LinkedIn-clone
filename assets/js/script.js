7cb3c012bb3efc516a5c8c4933bcf1322eae7708
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-14 18:25:07 +0530

    LinkedIn Clone completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 let profileMenu = document.getElementById('profileMenu');
+let sideActivity = document.getElementById('sidebarActivity');
+let moreLink = document.getElementById('showMoreLink');
 
+// toggle menu
 function toggleMenu() {
     profileMenu.classList.toggle('open-menu');
+}
+
+// toggle activity
+function toggleActivity() {
+    sideActivity.classList.toggle('open-activity');
+
+    if(sideActivity.classList.contains("open-activity")) {
+        moreLink.innerHTML = "Show less <b>-</b>";
+    } else {
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
 }
